7f0df83f6d7f4277f2857da7488927482ce29454
JacobsRose <112167889+JacobsRose@users.noreply.github.com>
2023-09-28 13:31:25 +0300

    First upload

---
+++ lua/jacobsrose/overrides.lua
@@ -0,0 +1,9 @@
+
+list.Add( "OverrideMaterials", "jacobsrose/metal/aluminium/scaffed/pbr" )
+list.Add( "OverrideMaterials", "jacobsrose/metal/steel/streaked/pbr" )
+list.Add( "OverrideMaterials", "jacobsrose/metal/steel/used/pbr" )
+list.Add( "OverrideMaterials", "jacobsrose/fabric/twill/pbr" )
+list.Add( "OverrideMaterials", "jacobsrose/leather/018/pbr" )
+list.Add( "OverrideMaterials", "jacobsrose/leather/035d/pbr" )
+list.Add( "OverrideMaterials", "jacobsrose/leather/033b/pbr" )
+list.Add( "OverrideMaterials", "jacobsrose/leather/035b/pbr" )
